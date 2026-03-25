7c7ef6371b4e14f76e3cac8dae27a5e4774d8344
estilobaby96-coder <estilobaby96@gmail.com>
2026-03-25 15:35:08 -0300

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,25 +1,30 @@
 import os
 import psycopg2
 import psycopg2.extras
+from contextlib import contextmanager
 from dotenv import load_dotenv
 
 load_dotenv()
-NEON_URL = os.getenv("NEON_URL")
+
+NEON_URL = os.getenv("NEON_URL", "")
 
 def get_connection():
-        return psycopg2.connect(NEON_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+            return psycopg2.connect(NEON_URL, cursor_factory=psycopg2.extras.RealDictCursor)
 
 class DatabaseSession:
-        def __init__(self):
-                    self.conn = None
-                    self.cur = None
+            def __init__(self):
+                            self.conn = None
+                            self.cur = None
 
-        def __enter__(self):
-                    self.conn = get_connection()
-                    self.cur = self.conn.cursor()
-                    return self
+            def __enter__(self):
+                            self.conn = get_connection()
+                            self.cur = self.conn.cursor()
+                            return self.cur
 
-        def __exit__(self, et, ev, tb):
-                    if self.cur: self.cur.close()
-                                if self.conn: self.conn.close()
-                                    
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                            if exc_type:
+                                                self.conn.rollback()
+            else:
+                    self.conn.commit()
+                    self.cur.close()
+            self.conn.close()

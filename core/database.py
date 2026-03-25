534d27eed36ed50a8a516b18114e8a7a830fe642
estilobaby96-coder <estilobaby96@gmail.com>
2026-03-25 14:03:54 -0300

    fix: core/database.py syntax error

--- core/database.py
+++ core/database.py
@@ -1,30 +1,31 @@
 import os
 import psycopg2
 import psycopg2.extras
-from contextlib import contextmanager
 from dotenv import load_dotenv
 
 load_dotenv()
 
 NEON_URL = os.getenv("NEON_URL", "")
 
+
 def get_connection():
-      return psycopg2.connect(NEON_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+          return psycopg2.connect(NEON_URL, cursor_factory=psycopg2.extras.RealDictCursor)
+
 
 class DatabaseSession:
-      def __init__(self):
-                self.conn = None
-                self.cur = None
+          def __init__(self):
+                        self.conn = None
+                        self.cur = None
 
-      def __enter__(self):
-                self.conn = get_connection()
-                self.cur = self.conn.cursor()
-                return self.cur
+          def __enter__(self):
+                        self.conn = get_connection()
+                        self.cur = self.conn.cursor()
+                        return self.cur
 
-      def __exit__(self, exc_type, exc_val, exc_tb):
-                if exc_type:
-                              self.conn.rollback()
+          def __exit__(self, exc_type, exc_val, exc_tb):
+                        if exc_type:
+                                          self.conn.rollback()
 else:
             self.conn.commit()
-          self.cur.close()
+              self.cur.close()
         self.conn.close()
